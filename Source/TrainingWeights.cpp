3c54785657956b65432569d3818d6203bc8890a6
jeanpierrethach <jeanpierre.thach@gmail.com>
2018-04-25 11:53:23 -0400

    added weight class for strategy purposes

---
+++ Source/TrainingWeights.cpp
@@ -0,0 +1,17 @@
+#include "TrainingWeights.h"
+
+
+namespace TrainingWeights
+{
+	namespace AttackTimers
+	{
+		int TimeInterval = 500;
+		int StartInterval = 12000;
+		int EndInterval = 15000;
+	}
+
+	namespace EndScore
+	{
+		//std::map<int, int> ResultsScore;
+	}
+}
---
+++ Source/TrainingWeights.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <map>
+
+namespace TrainingWeights
+{
+	namespace AttackTimers
+	{
+		extern int TimeInterval;
+		extern int StartInterval;
+		extern int EndInterval;
+	}
+
+	namespace EndScore
+	{
+		//extern std::map<int, int>;
+	}
+}
